eec1e8a11969fa6f104ad867221ec609aeb12d8b
zmojtaba <zare.m@yaftar.ir>
2023-05-08 17:17:19 +0430

    dev branche added

--- DressShop/DressShop/asgi.py
+++ DressShop/DressShop/asgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DressShop.settings')
