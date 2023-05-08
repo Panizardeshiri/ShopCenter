08c6a690590c795bdea057ec2ccdd69849ba0b48
zmojtaba <zare.m@yaftar.ir>
2023-05-08 17:33:01 +0430

    .env problem

--- DressShop/DressShop/settings.py
+++ DressShop/DressShop/settings.py
@@ -118,6 +118,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
