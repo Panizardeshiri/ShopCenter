c590cd9489f3daab44ae47d4e2f056296bb152f1
zmojtaba <mojtaba.zare8131@gmail.com>
2023-08-08 10:19:35 -0400

    project structure change to back and forntend

--- Front/Shop/src/app/core/header/search/filterproducts.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterproductsPipe } from './filterproducts.pipe';
-
-describe('FilterproductsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterproductsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- Shop/products/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path,include
-app_name='products'
-
-urlpatterns = [
-    path('api-vi/',include('products.api_vi.urls'),name = 'api-products')
-]
